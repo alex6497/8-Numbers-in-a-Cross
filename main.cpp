3590bfac9674339e8efb58bb89d8d1fba0f26f46
Alex Santisteban <alexjr6497@hotmail.com>
2020-02-20 15:45:12 +0000

    description of game

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+using namespace std;
+
+/*Imagine a cross with 6 boxes from top to bottom 2x2 and 1 box on each center end. making 8 boxes total.
+
+You may only input 1 number between one and eight. Each number can only appear once and its box cannot be next to its next or previous number's box vertically, horizontally, or diagonally.
+*/
 
 int main() {
-  std::cout << "Hello World!\n";
+  
+  
+
 }
